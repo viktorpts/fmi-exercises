45d2bd1d3a931ac07ff3f745101cc6d50f91d06c
Viktor Kostadinov <viktor.pts@abv.bg>
2019-11-19 16:21:32 +0200

    added more basic operations

--- CompGeom/simple.cpp
+++ CompGeom/simple.cpp
@@ -31,6 +31,12 @@ double orientedArea(vec2d<> A1, vec2d<> A2, vec2d<> A3)
     return ((A2 - A1) ^ (A3 - A1));
 }
 
+vec2d<> projectUtoV(vec2d<> u, vec2d<> v)
+{
+    auto scalar = (u * v) / (v.x * v.x + v.y * v.y);
+    return v * scalar;
+}
+
 bool isInTriangle(vec2d<> point, vec2d<> node, vec2d<> next, vec2d<> prev)
 {
     int sign = sgn(orientedArea(node, next, prev));
@@ -39,6 +45,27 @@ bool isInTriangle(vec2d<> point, vec2d<> node, vec2d<> next, vec2d<> prev)
     int s3 = sgn(orientedArea(point, prev, next));
 
     return sign == s1 && sign == s2 & sign == s3;
+}
+
+bool pointOnLine(vec2d<> point, vec2d<> u, vec2d<> v) {
+    auto ux = point - u;
+    auto vx = point - v;
+    return (ux ^ vx) == 0;
+}
+
+bool pointInSegment(vec2d<> point, vec2d<> u, vec2d<> v)
+{
+    // It's assumed the colinearility has been determined elsewhere
+    auto ux = point - u;
+    auto vx = point - v;
+    return ux * vx <= 0;
+}
+
+bool segmentIntersect(vec2d<> p, vec2d<> q, vec2d<> u, vec2d<> v)
+{
+    // Solution does not check for colinearity
+    // Returns true if one node lies within the other segment
+    return sgn(orientedArea(p, q, u)) != sgn(orientedArea(p, q, v)) && sgn(orientedArea(u, v, p)) != sgn(orientedArea(u, v, q));
 }
 
 bool isConvex(vector<vec2d<>> polygon)
---
+++ CompGeom/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "vecta.h"
+#include "simple.h"
+
+using namespace std;
+using namespace vecta;
+
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
